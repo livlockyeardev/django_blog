741a8c5d6616a3bf7fbfd2518334c02926aada14
livlockyeardev <livlockyear@gmail.com>
2026-02-01 19:00:12 +0000

    add about page for website

---
+++ about/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+from .models import About
+# Create your views here.
+def about(request):
+    """
+    Display an individual :model:`about.About`.
+
+    **Context**
+
+    ``about``
+        An instance of :model:`about.About`.
+
+    **Template:**
+
+    :template:`about/about.html`
+    """
+
+    about = About.objects.order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
